01f895afceb454ea5ed9c302cee251cdf43d1c7d
L1nberal <92567535+L1nberal@users.noreply.github.com>
2022-11-07 11:03:25 +0700

    delete other-login-methods-user conten-type and add from field to users content-type

--- src/api/other-login-methods-user/routes/other-login-methods-user.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * other-login-methods-user router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::other-login-methods-user.other-login-methods-user');
--- src/api/other-login-methods-user/services/other-login-methods-user.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * other-login-methods-user service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::other-login-methods-user.other-login-methods-user');
